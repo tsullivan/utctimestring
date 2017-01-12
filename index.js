6e368b691a90f3759d586a3a9999eafbc7d8ea92
Timothy Sullivan <tsullivan@elastic.co>
2017-01-11 23:06:00 -0700

    date and spread params and iterate

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 #! /usr/bin/env node
 const moment = require('moment')
-const now = moment.utc();
-const formats = {
-  iso: now.format(),
-  epoch_millis: now.valueOf(),
-  filename: now.format('YYYYMMDD_HHmm')
-};
-console.log(formats);
+const argv = require('yargs').argv;
+
+function runIt(time) {
+  time = moment.utc(time);
+  return {
+    iso: time.format(),
+    epoch_millis: time.valueOf(),
+    filename: time.format('YYYYMMDD_HHmm')
+  };
+}
+
+const start = moment.utc().valueOf();
+
+if (argv.date && argv.spread) {
+  const earliest = moment.utc(argv.date).valueOf();
+  const spread = argv.spread;
+  const delta = start - earliest;
+  const increment = Math.floor(delta / (spread - 1));
+  let iterations = spread;
+  let time = earliest;
+
+  while (iterations > 0) {
+    const result = runIt(time, spread, increment);
+    const index = spread - iterations + 1;
+    console.log({
+      [index]: result
+    });
+    time += increment;
+    iterations--;
+  }
+} else {
+  let dateInput;
+
+  if (argv.date) {
+    dateInput = argv.date;
+  } else {
+    dateInput = start;
+  }
+
+  console.log(runIt(dateInput));
+}
